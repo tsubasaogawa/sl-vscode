12725b93a6bbc622c887c75304ee325e0febef6e
tsubasaogawa <hurikake@gmail.com>
2022-07-30 17:55:21 +0900

    make it possible to get part of sl

--- src/sl.js
+++ src/sl.js
@@ -10,6 +10,12 @@ function run() {
     const position = editor.selection.active;
 
     editor.edit((edit => {
-        edit.insert(new vscode.Position(position.line, 0), define.D51.STR);
+        edit.insert(new vscode.Position(position.line, 0), getPart(define.D51.STR, 0, 40));
     }));
+}
+
+function getPart(sl, indexStart, indexEnd = undefined) {
+    const result = sl.split('\n').map(line => line.slice(indexStart, indexEnd)).join('\n');
+    console.log(result);
+    return result;
 }
