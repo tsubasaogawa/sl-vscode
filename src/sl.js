7204b748786fba0bd7ce57494fecf57632de1d5d
tsubasaogawa <hurikake@gmail.com>
2022-08-27 13:10:06 +0900

    make it possible to move a train

--- src/sl.js
+++ src/sl.js
@@ -1,16 +1,32 @@
 const vscode = require('vscode');
 const define = require('./define');
+const { setTimeout } = require('timers/promises');
+
+const windowWidth = 60;
 
 module.exports = {
     run,
 }
 
-function run() {
-    const editor = vscode.window.activeTextEditor;
+async function run() {
+    let editor = vscode.window.activeTextEditor;
     const position = editor.selection.active;
+    const startLine = position.line;
 
-    editor.edit((edit => {
-        edit.insert(new vscode.Position(position.line, 0), getPart(define.D51.STR, 0, 40));
+    for (let i = 0; i < 60; i++) {
+        await draw(editor, startLine, i);
+        await setTimeout(100);
+    }
+}
+
+async function draw(editor, startLine, i) {
+    const startPos = new vscode.Position(startLine, 0);
+    const selection = new vscode.Selection(
+        startPos,
+        new vscode.Position(startLine + 9, windowWidth))
+        ;
+    await editor.edit((edit => {
+        edit.replace(selection, getPart(define.D51.STR, i, windowWidth + i));
     }));
 }
 
