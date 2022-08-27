60e4c87f00293e00116af391282ec4321b51c01d
tsubasaogawa <hurikake@gmail.com>
2022-08-28 01:28:46 +0900

    add coal

--- src/sl.js
+++ src/sl.js
@@ -20,12 +20,13 @@ async function run() {
 }
 
 async function draw(editor, startLine, i) {
+    const train = `${define.D51.smoke(i)}${define.D51.STR}${define.D51.wheel(i)}`;
+
     const selection = new vscode.Selection(
         new vscode.Position(startLine, 0),
-        new vscode.Position(startLine + define.D51.height(), windowWidth))
-        ;
+        new vscode.Position(startLine + define.D51.height(), windowWidth)
+    );
     await editor.edit((edit => {
-        const train = docking('', define.D51.STR, define.D51.wheel(i));
         edit.replace(selection, getPart(train, i, windowWidth + i));
     }));
 }
@@ -34,8 +35,4 @@ function getPart(sl, indexStart, indexEnd = undefined) {
     const result = sl.split('\n').map(line => line.slice(indexStart, indexEnd)).join('\n');
     console.log(result);
     return result;
-}
-
-function docking(top, body, bottom) {
-    return `${top}${body}${bottom}`;
 }
