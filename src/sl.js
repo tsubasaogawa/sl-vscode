f75ccf09a092c7a1c511792d12590cb58c831684
tsubasaogawa <hurikake@gmail.com>
2022-08-28 22:27:53 +0900

    tuning

--- src/sl.js
+++ src/sl.js
@@ -10,27 +10,30 @@ async function run() {
     let editor = vscode.window.activeTextEditor;
     const position = editor.selection.active;
     const startLine = position.line;
+    const config = vscode.workspace.getConfiguration('sl');
 
     for (let i = 0; i < define.D51.width; i++) {
-        await draw(editor, startLine, i);
-        await setTimeout(refreshRate);
+        await draw(editor, config, startLine, i);
+        await setTimeout(config.refreshRateMs);
     }
 }
 
-async function draw(editor, startLine, i) {
-    const train = `${define.D51.smoke(i)}${define.D51.STR}${define.D51.wheel(i)}`;
+async function draw(editor, config, startLine, i) {
+    const replaceByTrain = ((edit) => {
+        const trainRange = new vscode.Range(
+            new vscode.Position(startLine, 0),
+            new vscode.Position(startLine + define.D51.height(), config.windowWidth)
+        );
+        const train = `${define.D51.smoke(i)}${define.D51.STR}${define.D51.wheel(i)}`;
+        const padding = ' '.repeat(
+            i <= config.windowWidth ? config.windowWidth - i : 0
+        );
+        edit.replace(trainRange, getPart(train, i, config.windowWidth + i, padding));
+    });
 
-    const selection = new vscode.Selection(
-        new vscode.Position(startLine, 0),
-        new vscode.Position(startLine + define.D51.height(), windowWidth)
-    );
-    await editor.edit((edit => {
-        edit.replace(selection, getPart(train, i, windowWidth + i));
-    }));
+    await editor.edit((edit) => replaceByTrain(edit), { undoStopBefore: i == 0 });
 }
 
-function getPart(sl, indexStart, indexEnd = undefined) {
-    const result = sl.split('\n').map(line => line.slice(indexStart, indexEnd)).join('\n');
-    console.log(result);
-    return result;
+function getPart(sl, indexStart, indexEnd = undefined, padding = '') {
+    return sl.split('\n').map(line => (padding + line).slice(indexStart, indexEnd)).join('\n');
 }
